301ea2b89b59cf7b8cb68089941c0dee1de243fd
Matt-Fulbrite <Matt-Fulbrite>
2023-09-21 10:12:29 -0400

    first commit

--- index.js
+++ index.js
@@ -80,11 +80,10 @@ app.put('/api/digimon/:id', async (req, res, next) => {
     }
 });
 
-//Errors, custom 404
 app.use('*', (req, res, next) => {
     res.status(404).send('404 Error');
 });
-//Errors error-handler
+
 app.use((err, req, res, next) => {
     res.status(500).send(err.message);
 });
@@ -106,7 +105,7 @@ const start = async () => {
     `;
 
     await client.query(SQL);
-    console.log('tables created and data seeded');
+    
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
